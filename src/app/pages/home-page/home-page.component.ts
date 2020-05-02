fc1196b0fcc06cad8f7b178c14476e8f7acfe1a8
Daniel Freitas <daniel.goncalves@ibm.com.br>
2020-05-02 13:33:20 -0300

    First Commit

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+  displayAboutMe: Boolean = false; 
+  hoverIndex = 0;
+  constructor() { 
+    setInterval(() => {
+      this.hoverIndex = (this.hoverIndex + 1) % 5;
+    }, 750);
+  }
+
+  ngOnInit(): void {
+    console.log('HomePage Loaded');
+    let aboutMe = document.getElementById("aboutme");
+    aboutMe.hidden = true;
+  }
+
+  viewDidLoad() {
+ 
+  }
+
+  showAboutMe() {
+    this.displayAboutMe = true;
+  }
+
+  hideAboutMe() {
+    this.displayAboutMe = false;
+  }
+
+  getAboutMeClass() {
+    return this.displayAboutMe? 'about-me-show' : 'about-me-hide';
+  }
+
+
+  getColor() {
+    switch(this.hoverIndex) {
+      case 0: return 'blue-color';
+      case 1: return 'green-color';
+      case 2: return 'red-color';
+      case 3: return 'pink-color';
+      case 4: return 'yellow-color';
+    }
+  }
+
+  getMagicLinkClass() {
+    return 'magic-link ' + this.getColor();
+  }
+}
